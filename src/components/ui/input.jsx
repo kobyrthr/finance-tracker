3ed4476238a5e4415bf7517aac39e7315c870975
kobyrthr <kobyrthr@gmail.com>
2025-03-29 18:27:17 +0800

    add hover style

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -7,7 +7,7 @@ const Input = React.forwardRef(({ className, type, ...props }, ref) => {
     <input
       type={type ?? 'text'}
       className={cn(
-        'flex h-[45px] w-full rounded-[var(--spacing-100)] bg-white px-5 py-3 transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-beige-500 placeholder:text-beige-500 disabled:cursor-not-allowed disabled:opacity-50  text-grey-900 border border-beige-500 focus-visible:outline-none font-normal text-[14px] leading-[150%] tracking-[0px]',
+        'flex h-[45px] w-full rounded-[var(--spacing-100)] bg-white px-5 py-3 transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-beige-500 placeholder:text-beige-500 disabled:cursor-not-allowed disabled:opacity-50  text-grey-900 border border-grey-500 hover:border-grey-900 focus:border-grey-900 focus-visible:outline-none font-normal text-[14px] leading-[150%] tracking-[0px]',
         className
       )}
       ref={ref}
